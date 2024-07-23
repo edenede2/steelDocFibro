bdbcb7f559e3ebf311ff012fdbe7dcd7bdecc2c1
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-07-23 12:36:53 +0300

    try adding

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.lib.pagesizes import letter
 from io import BytesIO
 import datetime
+import pandas as pd
+from pathlib import Path
+import time
+from streamlit_drawable_canvas import st_canvas
+from streamlit_pdf_viewer import pdf_viewer
+import re
+from PIL import Image
+import base64
+
+import os
+
 
 
 # Load the PDF
@@ -15,13 +26,13 @@ font_path = "OpenSans-VariableFont_wdth,wght.ttf"  # Adjust this to the path of 
 def reversing_chars(s):
     return s[::-1]
 
-def create_pdf(fields, table_data):
+def create_pdf(fields, table_data, signature_img=None):
     packet = BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
     
     # Register and set the font
     pdfmetrics.registerFont(TTFont('FreeSans', font_path))
-    can.setFont('FreeSans', 30)
+    can.setFont('FreeSans', 10)
 
 
     # Adjusted coordinates (from pdfplumber output, converting from points to position in the document)
@@ -36,17 +47,72 @@ def create_pdf(fields, table_data):
     can.drawString(x_name-230, y_name - 28, f"{fields['weight']}")
     
 
-    
+    ros_counter = 0
 
     # Fill table (adjust coordinates as needed)
     for row in table_data:
-        if row['answer']:
-            can.drawString(x_name + 100, y_name - 185, 'X')
-        can.drawString(x_name + 200, start_y, 'כן' if row['answer'] else 'לא')
-        if not row['answer']:
-            can.drawString(x_name + 300, start_y, row['details'])
-        start_y -= 20
-    
+        if ros_counter < 12:
+            if row['answer'] == 'כן':
+                can.drawString(x_name + 10, y_name - 185, 'X')
+            elif row['answer'] == 'לא':
+                can.drawString(x_name + 30, y_name - 185, 'X')
+            elif row['answer'] == 'לא יודע/ת':
+                can.drawString(x_name - 10, y_name - 185, 'X')
+            
+            if row['details'] != '':
+                can.drawString(x_name - 250, y_name - 185, reversing_chars(row['details']))
+
+            y_name -= 20
+            ros_counter += 1
+
+    can.showPage()
+
+    x_name = 325
+    y_name = 900
+
+    table_data = table_data[12:]
+
+    for row in table_data:
+        if ros_counter >= 12 and ros_counter < 28:
+            if row['answer'] == 'כן':
+                can.drawString(x_name + 10, y_name - 185, 'X')
+            elif row['answer'] == 'לא':
+                can.drawString(x_name + 30, y_name - 185, 'X')
+            elif row['answer'] == 'לא יודע/ת':
+                can.drawString(x_name - 10, y_name - 185, 'X')
+
+            if row['details'] != '':
+                can.drawString(x_name - 250, y_name - 185, reversing_chars(row['details']))
+
+            y_name -= 20
+            ros_counter += 1
+
+    can.showPage()
+
+    x_name = 325
+    y_name = 430
+
+    table_data = table_data[28:]
+
+    for row in table_data:
+        if ros_counter >= 28:
+            if row['answer'] == 'כן':
+                can.drawString(x_name + 10, y_name - 185, 'X')
+            elif row['answer'] == 'לא':
+                can.drawString(x_name + 30, y_name - 185, 'X')
+            elif row['answer'] == 'לא יודע/ת':
+                can.drawString(x_name - 10, y_name - 185, 'X')
+
+            if row['details'] != '':
+                can.drawString(x_name - 250, y_name - 185, reversing_chars(row['details']))
+
+            y_name -= 20
+            ros_counter += 1
+                
+    if signature_img:
+        can.drawImage(signature_img, 50, 50, width=100, height=50)
+
+
     can.save()
     
     # Move to the beginning of the StringIO buffer
@@ -61,6 +127,15 @@ def create_pdf(fields, table_data):
     page = existing_pdf.pages[0]
     page.merge_page(new_pdf.pages[0])
     output.add_page(page)
+
+    if len(existing_pdf.pages) > 1:
+        second_page = existing_pdf.pages[1]
+        second_page.merge_page(new_pdf.pages[1])
+        output.add_page(second_page)
+    else:
+        output.add_page(new_pdf.pages[1])
+
+
     
     # Finally, write "output" to a real file
     output_stream = BytesIO()
@@ -68,6 +143,27 @@ def create_pdf(fields, table_data):
     output_stream.seek(0)
     
     return output_stream
+
+
+def signature():
+    try:
+        Path("tmp/").mkdir()
+    except FileExistsError:
+        pass
+
+    if st.session_state.get("button_id", "") == "":
+        st.session_state["button_id"] = re.sub("\d+", "", str(uuid.uuid4()).replace("-", ""))
+
+    button_id = st.session_state["button_id"]
+    file_path = f"tmp/{button_id}.png"
+
+    data = st_canvas(update_streamlit=False, key="png_export")
+    if data is not None and data.image_data is not None:
+        img_data = data.image_data
+        im = Image.fromarray(img_data.astype("uint8"), mode="RGBA")
+        im.save(file_path, "PNG")
+        return file_path
+
 
 st.title("טופס בטיחות - MRI")
 
@@ -102,17 +198,85 @@ st.write("""
 """
 )
 
-
-# Table data
-table_data = [
-    {'question': 'קוצב לב', 'answer': st.checkbox('קוצב לב', key='q1'), 'details': st.text_input('פרטים (קוצב לב)', key='d1') if not st.checkbox('קוצב לב', key='a1') else ''},
-    {'question': 'מסתם לב מלאכותי', 'answer': st.checkbox('מסתם לב מלאכותי', key='q2'), 'details': st.text_input('פרטים (מסתם לב מלאכותי)', key='d2') if not st.checkbox('מסתם לב מלאכותי', key='a2') else ''}
-    # Add other fields similarly with unique keys
+questions_list= [
+    'קוצב לב',
+    'מסתם לב מלאכותי',
+    'שנט במערכת העצבים/אחר',
+    'סיכות מתכתיות לאחר ניתוח מפרצת ראש',
+    '(neurostimulator) מגרי עצבים',
+    'כתר מתכת/גשר',
+    'רסיס מתכת (לאחר פציעה)',
+    'סיכות, מהדקים מתכתיים, פילטרים, סלילים לאחר ניתוח וטיפולים בכלי דם ',
+    'מוט מתכת, פלטת ברגים, מסמרים לאחר ניתוחים אורתופדים',
+    'מפרק מלאכותי',
+    'אלקטרודות',
+    'שתל של רשת מתכתית',
+    'מותחן הזרקה לשד',
+    'שתלים מכל סוג שהוא',
+    'קעקועים (ציין גודל, מיקום וצבע, אפשר גם לשלוח תצלום לצוות המחקר)',
+    'פירסינג, עגילים שאי אפשר להסיר',
+    'permanent makeup איפור קבוע',
+    'האם עברת ארטרוסקופיה ובאיזו ברך',
+    'האם עברת/ה ניתוחים קודמים',
+    'האם היית/ה מעורב בתאונת דרכים?',
+    'האם עבדת עם מתכת ללא הגנה על העיניים',
+    'האם מתכת אי פעם נכנסה לך לעין',
+    'האם יש לך בגדים מבדים אנטי בקטריאליים',
+    'בעבר (ציין תאריך, מטרה, מכון) MRI האם עברת/ה סריקת',
+    'האם יש עליך או בתוכך אביזר ממתכת',
+    'האם את/ה סובל מקלסטרופוביה',
+    'האם יש לך משקפיים',
+    'האם יש בגופך התקן תוך רחמי',
+    'האם את בהריון'
 ]
 
-if st.button("Save as PDF", key='do1'):
-    pdf_stream = create_pdf(fields, table_data)
-    st.download_button(label="Download PDF", data=pdf_stream, file_name="edited_document.pdf", mime="application/pdf")
-if st.button("Save as PDF",key='do2'):
-    pdf_stream = create_pdf(fields, table_data)
-    st.download_button(label="Download PDF", data=pdf_stream, file_name="edited_document.pdf", mime="application/pdf")
+
+
+with st.form("table_form", key='table_form'):
+    table_data = []
+    st.write("""
+ציין האם יש בתוך/על גופך את הפרטים הבאים:
+    """)
+    for i in range(len(questions_list)):
+        row = {
+            'question_obj': st.write(questions_list[i]),
+            'question': st.write(f"סמנו את המתאים", key=f"question_labels_{i}"), 
+            'answer': st.radio("", options=['כן', 'לא', 'לא יודע/ת'], key=f"answer_{i}"),
+            'details_lab': st.text_input("אם כן / לא יודע/ת הוסיפו פרטים + תאריך של האירוע", key=f"details_lab_{i}"),
+            'details': st.text_input("פרטים נוספים", key=f"details_{i}")
+        }
+
+        table_data.append(row)
+    submit_button = st.form_submit_button("שלח", key='submit_button')
+
+
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=2,
+    stroke_color="#000000",
+    background_color="#FFFFFF",
+    height=150,
+    width=300,
+    drawing_mode="freedraw",
+    key="canvas",
+)
+
+
+# Create PDF
+if submit_button:
+    if canvas_result is not None and canvas_result.image_data is not None:
+        signature_img = signature()
+        pdf_stream = create_pdf(fields, table_data, signature_img=signature_img)
+        if pdf_stream is not None:
+            binarystream = pdf_stream.getvalue()
+            pdf_viewer(input_file=binarystream, height=800)
+            st.download_button(
+                label="הורדת טופס",
+                data=binarystream,
+                file_name="output.pdf",
+                mime="application/pdf",
+            )
+            
+        
