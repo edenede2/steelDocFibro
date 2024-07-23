930306a8fa4c2d429264fd3db3b2af8e141e2c69
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-07-23 13:49:22 +0300

    s

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def create_pdf(fields, table_data, signature_img=None):
             elif ros_counter == 27:
                 y_name -= 29
             elif ros_counter == 28:
-                y_name -= 42
+                y_name -= 47
             elif ros_counter == 29:
                 y_name -= 29
             else:
