cfa210f3a9d87605b5a61a67d98d5d6aead56075
edenede2 <131803027+edenede2@users.noreply.github.com>
2024-07-23 13:29:46 +0300

    s

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def create_pdf(fields, table_data, signature_img=None):
                 if row['details'] != '':
                     can.drawString(x_name - 250, y_name - 185, reversing_chars(row['details']))
 
-                y_name -= 38
+                y_name -= 40
 
             elif ros_counter == 10:
                 if row['answer'] == 'כן':
@@ -97,7 +97,7 @@ def create_pdf(fields, table_data, signature_img=None):
                 if row['details'] != '':
                     can.drawString(x_name - 250, y_name - 185, reversing_chars(row['details']))
 
-                y_name -= 25
+                y_name -= 35
             else:
                 if row['answer'] == 'כן':
                     can.drawString(x_name + 10, y_name - 185, 'X')
